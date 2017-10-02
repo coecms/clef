07d0045d3e0abf4ee36df7ec2460624a2295b77c
Scott Wales <scott.wales@unimelb.edu.au>
2017-10-02 15:57:56 +1100

    Fix metadata sample

--- db/nci.sql
+++ db/nci.sql
@@ -30,6 +30,11 @@ CREATE TABLE checksums (
 
 
 -- Read sample data
+/*
+\copy (with x as (select * from esgf_filter limit 5) select y.* from metadata as y join x on md_hash = file_id) to db/metadata_sample.txt 
+\copy (with x as (select * from esgf_filter limit 5) select y.* from checksums as y join x on ch_hash = file_id) to db/checksums_sample.txt 
+\copy (with x as (select * from esgf_filter limit 5) select y.* from paths as y join x on pa_hash = file_id) to db/paths_sample.txt 
+*/
 \copy metadata  from 'db/metadata_sample.txt'
 \copy paths     from 'db/paths_sample.txt'
 \copy checksums from 'db/checksums_sample.txt'
