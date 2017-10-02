f2adb0584fc12d191d5a32a5839923f9006de8db
Scott Wales <scott.wales@unimelb.edu.au>
2017-10-02 15:25:18 +1100

    Get url from environment

--- conftest.py
+++ conftest.py
@@ -2,11 +2,12 @@
 from __future__ import print_function
 import pytest
 import arccssive2.db
+import os
 
 # Add test fixtures here so they are accessible from doctests
 @pytest.fixture(scope='session')
 def session():
-    arccssive2.db.connect(user='saw562')
+    arccssive2.db.connect(url = os.environ['ARCCSSIVE_URL'])
     session = arccssive2.db.Session()
     yield session
     session.rollback()
