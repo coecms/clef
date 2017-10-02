622d84b6907287a79382d8e0a4734351454d5f16
Scott Wales <scott.wales@unimelb.edu.au>
2017-10-02 11:18:32 +1100

    Adding extra tables

--- db/tables.sql
+++ db/tables.sql
@@ -4,7 +4,10 @@ CREATE OR REPLACE VIEW esgf_filter AS
     FROM paths
     WHERE
         pa_type = 'file'
-        AND pa_parents[6] = md5('/g/data1/ua6/unofficial-ESG-replica/tmp/tree')::uuid;
+        AND (
+            pa_parents[6] = md5('/g/data1/ua6/unofficial-ESG-replica/tmp/tree')::uuid
+            OR  pa_parents[4] = md5('/g/data1/ua6/authoritative')::uuid
+         );
 
 
 CREATE OR REPLACE VIEW dataset_metadata AS
@@ -39,8 +42,8 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS esgf_metadata_dataset_link AS
         )::uuid as dataset_id
     FROM dataset_metadata
     NATURAL JOIN esgf_filter;
-CREATE INDEX esgf_metadata_dataset_link_file_id ON esgf_metadata_dataset_link(file_id);
-CREATE INDEX esgf_metadata_dataset_link_dataset_id ON esgf_metadata_dataset_link(dataset_id);
+CREATE INDEX IF NOT EXISTS esgf_metadata_dataset_link_file_id ON esgf_metadata_dataset_link(file_id);
+CREATE INDEX IF NOT EXISTS esgf_metadata_dataset_link_dataset_id ON esgf_metadata_dataset_link(dataset_id);
 GRANT SELECT ON esgf_metadata_dataset_link TO PUBLIC;
 
 
@@ -63,4 +66,37 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS esgf_dataset AS
 CREATE INDEX IF NOT EXISTS esgf_dataset_dataset_id ON esgf_dataset(dataset_id);
 GRANT SELECT ON esgf_dataset TO PUBLIC;
     
-    
+/* Extra metadata not stored in the file itself
+ */
+CREATE TABLE IF NOT EXISTS extended_metadata (
+    file_id UUID PRIMARY KEY,
+    version TEXT,
+    variable TEXT,
+    period INT4RANGE  /* Date stored as a 8 character integer, e.g. 19800101
+                         to avoid calendar issues */
+    );
+GRANT SELECT ON extended_metadata TO PUBLIC;
+
+/* Users can add their own messages here
+ */
+CREATE TABLE IF NOT EXISTS errata (
+    id SERIAL PRIMARY KEY,
+    value TEXT,
+    added_by TEXT DEFAULT CURRENT_USER,
+    added_on TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+GRANT SELECT ON errata TO PUBLIC;
+GRANT INSERT (value) ON errata TO PUBLIC;
+
+/* Links an errata message to a file
+ */
+CREATE TABLE IF NOT EXISTS file_errata_link (
+    errata_id INTEGER REFERENCES errata(id) ON DELETE CASCADE,
+    file_id UUID,
+    added_by TEXT DEFAULT CURRENT_USER,
+    added_on TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (errata_id, file_id)
+    );
+GRANT SELECT ON file_errata_link TO PUBLIC;
+GRANT INSERT (errata_id, file_id) ON file_errata_link TO PUBLIC;
+
