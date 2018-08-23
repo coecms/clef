774f8bb0c3463dfd78a4a97c250f63e29a05e928
paolap@utas.edu.au <paolap@utas.edu.au>
2018-08-24 09:23:20 +1000

    reduced subcommands and added output options

--- arccssive2/cli.py
+++ arccssive2/cli.py
@@ -26,11 +26,25 @@ import six
 import os
 
 @click.group()
-def esgf():
-    """
-    Commands for searching ESGF
-    """
-    pass
+#@click.argument('project', nargs=1)
+@click.option('--search', is_flag=True, default=False, help="returns only ESGF search results")
+@click.option('--local', is_flag=True, default=False, help="returns only local files matching ESGF search")
+@click.option('--missing', is_flag=True, default=False, help="returns only missing files matching ESGF search")
+@click.option('--request', is_flag=True, default=False,
+               help="send NCI request to download missing files matching ESGF search")
+@click.pass_context
+#def esgf(ctx, project, search, local, missing, request):
+def esgf(ctx, search, local, missing, request):
+    ctx.obj={}
+    #ctx.obj['project']=project.upper()
+    ctx.obj['search']=search
+    ctx.obj['local']=local
+    ctx.obj['missing']=missing
+    ctx.obj['request']=request
+    
+    #if project in ['cmip5','CMIP5']:
+    #   ctx.forward(ctx,c5_local)     
+    #   ctx.invoke(ctx,c5_local()     
 
 def warning(message):
     print("WARNING: %s"%message, file=sys.stderr)
@@ -38,14 +52,14 @@ def warning(message):
 
 def cmip5_args(f):
     constraints = [
-        click.option('--ensemble', '-en', multiple=True, help="Constraint"),
+        click.option('--ensemble', '--member', '-en', 'ensemble', multiple=True, help="Constraint"),
         click.option('--experiment', '-e', multiple=True, help="Constraint"),
         click.option('--experiment_family',multiple=True, help="Constraint"),
-        click.option('--institute',multiple=True, help="Constraint"),
-        click.option('--cmor_table', '--mip', '-t', 'cmor_table', multiple=True, help="Constraint"),
+        click.option('--institution', 'institute', multiple=True, help="Constraint"),
+        click.option('--table', '--mip', '-t', 'cmor_table', multiple=True, help="Constraint"),
         click.option('--model', '-m', multiple=True, help="Constraint"),
-        click.option('--time_frequency',multiple=True, help="Constraint"),
-        click.option('--variable', '-v', multiple=True, help="Constraint"),
+        click.option('--frequency', 'time_frequency', multiple=True, help="Constraint"),
+        click.option('--variable', '-v', multiple=True, help="Constraint")
     ]
     for c in reversed(constraints):
         f = c(f)
@@ -72,195 +86,31 @@ def cmip6_args(f):
 # 
     constraints = [
         click.option('--variant_label', '-vl', multiple=True, help="Constraint"),
-        click.option('--member_id', '-mi', multiple=True, help="Constraint"),
-        click.option('--activity_id', '-mip', multiple=True, help="Constraint"),
-        click.option('--experiment_id', '-e', multiple=True, help="Constraint"),
+        click.option('--member', '-mi', 'member_id', multiple=True, help="Constraint"),
+        click.option('--activity', '-mip', 'activity_id', multiple=True, help="Constraint"),
+        click.option('--experiment', '-e', 'experiment_id', multiple=True, help="Constraint"),
         click.option('--sub_experiment_id', '-se', multiple=True, help="Constraint"),
         click.option('--source_type',multiple=True, help="Constraint"),
-        click.option('--institution_id',multiple=True, help="Constraint"),
-        click.option('--table_id', '-t', multiple=True, help="Constraint"),
+        click.option('--institution', 'institution_id', multiple=True, help="Constraint"),
+        click.option('--table', '-t', 'table_id', multiple=True, help="Constraint"),
         click.option('--model', '--source_id','-m', 'source_id', multiple=True, help="Constraint"),
         click.option('--frequency',multiple=True, help="Constraint"),
         click.option('--variable', 'variable_id', '-v', multiple=True, help="Constraint"),
+        click.option('--grid', '--grid_label', '-g', 'grid_label', multiple=True, help="Constraint"),
+        click.option('--resolution', '--nominal_resolution','-nr' , 'nominal_resolution', multiple=True, help="Constraint")
     ]
     for c in reversed(constraints):
         f = c(f)
     return f
 
-@esgf.command()
-@common_args
-@cmip5_args
-def cmip5(query, user, debug, distrib, replica, latest, format,
-        cf_standard_name,
-        ensemble,
-        experiment,
-        experiment_family,
-        institute,
-        cmor_table,
-        model,
-        realm,
-        time_frequency,
-        variable
-        ):
-    """
-    Search ESGF, returning matching file ids
-    """
-    if debug:
-        logging.basicConfig(level=logging.DEBUG)
-        logging.getLogger('sqlalchemy.engine').setLevel(level=logging.INFO)
 
-    connect(user=user)
-    s = Session()
-    
-    project='CMIP5'
 
-    q = find_checksum_id(' '.join(query),
-            distrib=distrib,
-            replica=replica,
-            latest=latest,
-            cf_standard_name=cf_standard_name,
-            ensemble=ensemble,
-            experiment=experiment,
-            experiment_family=experiment_family,
-            institute=institute,
-            cmor_table=cmor_table,
-            model=model,
-            project=project,
-            realm=realm,
-            time_frequency=time_frequency,
-            variable=variable
-            )
-
-    
-    for result in s.query(q):
-        print(result.id)
-
-@esgf.command()
-@common_args
-@cmip6_args
-def cmip6(query, user, debug, distrib, replica, latest, format,
-        cf_standard_name,
-        variant_label,
-        member_id,
-        experiment_id,
-        sub_experiment_id,
-        source_type,
-        institution_id,
-        table_id,
-        source_id,
-        realm,
-        frequency,
-        variable_id,
-        activity_id
-        ):
-    """
-    Search ESGF, returning matching file ids
-    """
-    if debug:
-        logging.basicConfig(level=logging.DEBUG)
-        logging.getLogger('sqlalchemy.engine').setLevel(level=logging.INFO)
-
-    connect(user=user)
-    s = Session()
-
-    q = find_checksum_id(' '.join(query),
-            distrib=distrib,
-            replica=replica,
-            latest=latest,
-            cf_standard_name=cf_standard_name,
-            variant_label=variant_label,
-            member_id=member_id,
-            experiment_id=experiment_id,
-            source_type=source_type,
-            institution_id=institution_id,
-            table_id=table_id,
-            source_id=source_id,
-            project='CMIP6',
-            realm=realm,
-            frequency=frequency,
-            variable_id=variable_id,
-            activity_id=activity_id
-            )
-    
-    for result in s.query(q):
-        print(result.id)
 
 @esgf.command()
 @common_args
 @cmip5_args
-def c5_missing(query, user, debug, distrib, replica, latest, format,
-        cf_standard_name,
-        ensemble,
-        experiment,
-        experiment_family,
-        institute,
-        cmor_table,
-        model,
-        realm,
-        time_frequency,
-        variable,
-        ):
-    """
-    Search ESGF to find files not downloaded to NCI
-    """
-    if debug:
-        logging.basicConfig(level=logging.DEBUG)
-        logging.getLogger('sqlalchemy.engine').setLevel(level=logging.INFO)
-
-    connect(user=user)
-    s = Session()
-
-    project=('CMIP5',)
-
-    dataset_constraints = {
-        'ensemble': ensemble,
-        'experiment': experiment,
-        'institute': institute,
-        'model': model,
-        'project': project,
-        'realm': realm,
-        'time_frequency': time_frequency,
-        'cmor_table': cmor_table,
-        }
-    terms = {}
-
-    # Add filters
-    for key, value in six.iteritems(dataset_constraints):
-        if len(value) > 0:
-            # If this key was filtered get a list of the matching values, used
-            # in the ESGF query
-            terms[key] = [x[0] for x in (s.query(getattr(C5Dataset,key))
-                .distinct()
-                .filter(getattr(C5Dataset,key).ilike(any_([x for x in value]))))]
-            if len(terms[key]) == 0:
-                warning("No matches found for %s: '%s'"%(key, value))
-                raise Exception
-
-    if len(variable) > 0:
-        terms['variable'] = [x[0] for x in (s.query(ExtendedMetadata.variable)
-            .distinct()
-            .filter(ExtendedMetadata.variable.ilike(any_([x for x in variable]))))]
-        if len(terms['variable']) == 0:
-            warning("No matches found for %s: '%s'"%('variable', value))
-            raise Exception
-
-    q = find_missing_id(s, ' '.join(query),
-            distrib=distrib,
-            replica=replica,
-            latest=(None if latest == 'all' else latest),
-            cf_standard_name=cf_standard_name,
-            experiment_family=experiment_family,
-            format=format,
-            **terms
-            )
-    
-    for result in q:
-        print(result[0])
-
-@esgf.command()
-@common_args
-@cmip5_args
-def c5_local(query, user, debug, distrib, replica, latest, format,
+@click.pass_context
+def cmip5(ctx, query, user, debug, distrib, replica, latest, format,
         cf_standard_name,
         ensemble,
         experiment,
@@ -304,6 +154,32 @@ def c5_local(query, user, debug, distrib, replica, latest, format,
         'time_frequency': time_frequency,
         'cmor_table': cmor_table,
         }
+
+    if ctx.obj['request']:
+        print('Sorry! This option is not yet implemented')
+        return
+
+    if ctx.obj['search']:
+        q = find_checksum_id(' '.join(query),
+            distrib=distrib,
+            replica=replica,
+            latest=latest,
+            cf_standard_name=cf_standard_name,
+            ensemble=ensemble,
+            experiment=experiment,
+            experiment_family=experiment_family,
+            institute=institute,
+            cmor_table=cmor_table,
+            model=model,
+            project=project,
+            realm=realm,
+            time_frequency=time_frequency,
+            variable=variable
+            )
+        for result in s.query(q):
+            print(result.id)
+        return
+
     terms = {}
     filters = []
 
@@ -328,8 +204,7 @@ def c5_local(query, user, debug, distrib, replica, latest, format,
     #if len(version) > 0:
     #    filters.append(ExtendedMetadata.version.ilike(any_(['%d'%x for x in version])))
 
-
-    q = find_local_path(s, query=None,
+    ql = find_local_path(s, query=None,
             distrib=True,
             replica=replica,
             latest=(None if latest == 'all' else latest),
@@ -338,90 +213,31 @@ def c5_local(query, user, debug, distrib, replica, latest, format,
             format=format,
             **terms
             )
+    if not ctx.obj['missing']:
+        for result in ql:
+            print(result[0])
+    if ctx.obj['local']: 
+        return
 
-    for result in q:
-        print(result[0])
-
-@esgf.command()
-@common_args
-@cmip6_args
-def c6_missing(query, user, debug, distrib, replica, latest, format,
-        cf_standard_name,
-        variant_label,
-        member_id,
-        experiment_id,
-        sub_experiment_id,
-        source_type,
-        institution_id,
-        table_id,
-        source_id,
-        realm,
-        frequency,
-        variable_id,
-        activity_id
-        ):
-    """
-    Search ESGF to find files not downloaded to NCI
-    """
-    if debug:
-        logging.basicConfig(level=logging.DEBUG)
-        logging.getLogger('sqlalchemy.engine').setLevel(level=logging.INFO)
-
-    connect(user=user)
-    s = Session()
-
-    dataset_constraints = {
-        'member_id': member_id,
-        'activity_id': activity_id,
-        'experiment_id': experiment_id,
-        'sub_experiment_id': sub_experiment_id,
-        'institution_id': institution_id,
-        'source_id': source_id,
-        'source_type': source_type,
-        'realm': realm,
-        'frequency': frequency,
-        'table_id': table_id,
-        }
-    terms = {}
-
-    # Add filters
-    for key, value in six.iteritems(dataset_constraints):
-        if len(value) > 0:
-            # If this key was filtered get a list of the matching values, used
-            # in the ESGF query
-            terms[key] = [x[0] for x in (s.query(getattr(C6Dataset,key))
-                .distinct()
-                .filter(getattr(C6Dataset,key).ilike(any_([x for x in value]))))]
-            if len(terms[key]) == 0:
-                warning("No matches found for %s: '%s'"%(key, value))
-                raise Exception
-
-    if len(variable_id) > 0:
-        terms['variable_id'] = [x[0] for x in (s.query(ExtendedMetadata.variable)
-            .distinct()
-            .filter(ExtendedMetadata.variable.ilike(any_([x for x in variable_id]))))]
-        if len(terms['variable_id']) == 0:
-            warning("No matches found for %s: '%s'"%('variable_id', value))
-            raise Exception
-
-    q = find_missing_id(s, ' '.join(query),
+    qm = find_missing_id(s, ' '.join(query),
             distrib=distrib,
             replica=replica,
             latest=(None if latest == 'all' else latest),
             cf_standard_name=cf_standard_name,
+            experiment_family=experiment_family,
             format=format,
-            project='CMIP6',
             **terms
             )
-    
-    for result in q:
-        print(result[0])
-
+    if qm.count() > 0:
+        print('Available on ESGF but not locally:')
+        for result in qm:
+            print(result[0])
 
 @esgf.command()
 @common_args
 @cmip6_args
-def c6_local(query, user, debug, distrib, replica, latest, format,
+@click.pass_context
+def cmip6(ctx,query, user, debug, distrib, replica, latest, format,
         cf_standard_name,
         variant_label,
         member_id,
@@ -434,7 +250,9 @@ def c6_local(query, user, debug, distrib, replica, latest, format,
         realm,
         frequency,
         variable_id,
-        activity_id
+        activity_id,
+        grid_label,
+        nominal_resolution
         ):
     """
     Search local database for files matching the given constraints
@@ -467,7 +285,39 @@ def c6_local(query, user, debug, distrib, replica, latest, format,
         'realm': realm,
         'frequency': frequency,
         'table_id': table_id,
+        'grid_label': grid_label,
+        'nominal_resolution': nominal_resolution
         }
+
+    if ctx.obj['request']:
+        print('Sorry! This option is not yet implemented')
+        return
+
+    if ctx.obj['search']:
+        q = find_checksum_id(' '.join(query),
+            distrib=distrib,
+            replica=replica,
+            latest=latest,
+            cf_standard_name=cf_standard_name,
+            variant_label=variant_label,
+            member_id=member_id,
+            experiment_id=experiment_id,
+            source_type=source_type,
+            institution_id=institution_id,
+            table_id=table_id,
+            source_id=source_id,
+            project='CMIP6',
+            realm=realm,
+            frequency=frequency,
+            variable_id=variable_id,
+            activity_id=activity_id,
+            grid_label=grid_label,
+            nominal_resolution=nominal_resolution
+            )
+        for result in s.query(q):
+            print(result.id)
+        return
+
     terms = {}
     filters = []
 
@@ -489,11 +339,7 @@ def c6_local(query, user, debug, distrib, replica, latest, format,
             .distinct()
             .filter(ExtendedMetadata.variable.ilike(any_([x for x in variable_id]))))]
 
-    #if len(version) > 0:
-    #    filters.append(ExtendedMetadata.version.ilike(any_(['%d'%x for x in version])))
-
-
-    q = find_local_path(s, query=None,
+    ql = find_local_path(s, query=None,
             distrib=True,
             replica=replica,
             latest=(None if latest == 'all' else latest),
@@ -502,7 +348,24 @@ def c6_local(query, user, debug, distrib, replica, latest, format,
             project='CMIP6',
             **terms
             )
+    if not ctx.obj['missing']:
+        for result in ql:
+            print(result[0])
+    if ctx.obj['local']: 
+        return
 
-    for result in q:
-        print(result[0])
+    qm = find_missing_id(s, ' '.join(query),
+            distrib=distrib,
+            replica=replica,
+            latest=(None if latest == 'all' else latest),
+            cf_standard_name=cf_standard_name,
+            format=format,
+            project='CMIP6',
+            **terms
+            )
+    
+    if qm.count() > 0:
+        print('Available on ESGF but not locally:')
+        for result in qm:
+            print(result[0])
 
