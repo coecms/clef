a86e53f726f12ae002dfc9781d169a832a11ef33
Scott Wales <scott.wales@unimelb.edu.au>
2018-08-28 10:14:27 +1000

    Cleanup arccssive2.esgf

--- arccssive2/esgf.py
+++ arccssive2/esgf.py
@@ -14,33 +14,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from __future__ import print_function
-import requests
+
 import json
-from sqlalchemy.sql import column, label
-from sqlalchemy.orm import aliased
+
+import requests
 from sqlalchemy import String, Float, Integer, or_, func
+from sqlalchemy.sql import column
+
+from .model import Path, Checksum
 from .pgvalues import values
-from .model import Path, Checksum, c5_metadata_dataset_link, c6_metadata_dataset_link
+
 
 def define_facets(project):
-    ''' Define available search facets based on project value: CMIP5 or CMIP6 '''
-    with open('../db/facets.json', 'r') as f: 
-       data = f.read()
-       fdict = json.loads(data)
+    """Define available search facets based on project value: CMIP5 or CMIP6"""
+    with open('../db/facets.json', 'r') as f:
+        data = f.read()
+        fdict = json.loads(data)
     if project == 'CMIP5':
-       facets = {v: None for v in fdict.values() if v != 'None'}
-       facets['project'] = 'CMIP5'
-    elif project == 'CMIP6': 
-       #facets = list(fdict.keys())
-       facets = {k: None for k in fdict.keys()}
-       facets['project'] = 'CMIP6'
+        facets = {v: None for v in fdict.values() if v != 'None'}
+        facets['project'] = 'CMIP5'
+    elif project == 'CMIP6':
+        facets = {k: None for k in fdict.keys()}
+        facets['project'] = 'CMIP6'
+    else:
+        facets = {}
     return facets
+
 
 def esgf_query(query, fields, limit=1000, offset=0, distrib=True, replica=False, latest=None, **kwargs):
     """
     Search the ESGF
     """
-    #facets = define_facets(project)
+    # facets = define_facets(project)
     if latest == 'all':
         latest = None
 
@@ -48,46 +53,45 @@ def esgf_query(query, fields, limit=1000, offset=0, distrib=True, replica=False,
         query = None
 
     params = {
-          'query': query,
-          'fields': fields,
-          'offset': offset,
-          'limit': limit,
-          'distrib': distrib,
-          'replica': replica,
-          'latest': latest, 
-          'type': 'File',
-          'format': 'application/solr+json',
-          } 
+        'query': query,
+        'fields': fields,
+        'offset': offset,
+        'limit': limit,
+        'distrib': distrib,
+        'replica': replica,
+        'latest': latest,
+        'type': 'File',
+        'format': 'application/solr+json',
+    }
     params.update(kwargs)
     r = requests.get('https://esgf-node.llnl.gov/esg-search/search',
-                     params = params )
+                     params=params)
 
     r.raise_for_status()
 
     return r.json()
 
+
 def link_to_esgf(query, **kwargs):
-    
-    constraints = {k: v for k,v in kwargs.items() if v != ()}
+    constraints = {k: v for k, v in kwargs.items() if v != ()}
     params = {
-            'query': query,
-            'fields': kwargs.get('fields',None),
-            'offset': kwargs.get('offset',None),
-            'limit': kwargs.get('limit',None),
-            'distrib': 'on' if kwargs.get('distrib',True) else None,
-            'replica': 'on' if kwargs.get('replica',False) else None,
-            'latest': 'on' if kwargs.get('latest',None) else None
-            }
+        'query': query,
+        'fields': kwargs.get('fields', None),
+        'offset': kwargs.get('offset', None),
+        'limit': kwargs.get('limit', None),
+        'distrib': 'on' if kwargs.get('distrib', True) else None,
+        'replica': 'on' if kwargs.get('replica', False) else None,
+        'latest': 'on' if kwargs.get('latest', None) else None
+    }
     params.update(constraints)
 
     endpoint = 'cmip5'
-    if params.get('project','').lower() == 'cmip6':
+    if params.get('project', '').lower() == 'cmip6':
         endpoint = 'cmip6'
 
-    r = requests.Request('GET','https://esgf-node.llnl.gov/search/%s'%endpoint,
-            params=params,
-            )
-    p = r.prepare()
+    r = requests.Request('GET', 'https://esgf-node.llnl.gov/search/%s' % endpoint,
+                         params=params,
+                         )
     return r.prepare().url
 
 
@@ -96,36 +100,39 @@ def find_checksum_id(query, project='CMIP6', **kwargs):
     Returns a sqlalchemy selectable containing the ESGF id and checksum for
     each query match
     """
-    constraints = {k: v for k,v in kwargs.items() if v != ()}
+    constraints = {k: v for k, v in kwargs.items() if v != ()}
     constraints['project'] = project
     response = esgf_query(query, 'checksum,id,dataset_id,title,version', **constraints)
 
     if response['response']['numFound'] == 0:
-        raise Exception('No matches found on ESGF, check at %s'%link_to_esgf(query, **constraints))
+        raise Exception('No matches found on ESGF, check at %s' % link_to_esgf(query, **constraints))
 
     if response['response']['numFound'] > int(response['responseHeader']['params']['rows']):
-        raise Exception('Too many results (%d), try limiting your search %s'%(response['response']['numFound'], link_to_esgf(query, **constraints)))
+        raise Exception('Too many results (%d), try limiting your search %s' % (
+                        response['response']['numFound'],
+                        link_to_esgf(query, **constraints)))
 
     table = values([
-            column('checksum', String),
-            column('id', String),
-            column('dataset_id', String),
-            column('title', String),
-            column('version', Integer),
-            column('score', Float),
-        ],
+        column('checksum', String),
+        column('id', String),
+        column('dataset_id', String),
+        column('title', String),
+        column('version', Integer),
+        column('score', Float),
+    ],
         *[(
             doc['checksum'][0],
             doc['id'],
-            doc['dataset_id'].split('|')[0], # Drop the server name
+            doc['dataset_id'].split('|')[0],  # Drop the server name
             doc['title'],
             doc['version'],
-            doc['score']) 
+            doc['score'])
             for doc in response['response']['docs']],
-        alias_name = 'esgf_query'
-        )
+        alias_name='esgf_query'
+    )
 
     return table
+
 
 def match_query(session, query, latest=None, **kwargs):
     values = find_checksum_id(query, latest=latest, **kwargs)
@@ -134,13 +141,14 @@ def match_query(session, query, latest=None, **kwargs):
         # Exact match on checksum
         return (values
                 .outerjoin(Checksum,
-                    or_(Checksum.md5 == values.c.checksum,
-                        Checksum.sha256 == values.c.checksum))
+                           or_(Checksum.md5 == values.c.checksum,
+                               Checksum.sha256 == values.c.checksum))
                 .outerjoin(Path))
     else:
         # Match on file name
-        #return values.outerjoin(Path, Path.path.like('%/'+values.c.title))
+        # return values.outerjoin(Path, Path.path.like('%/'+values.c.title))
         return values.outerjoin(Path, func.regexp_replace(Path.path, '^.*/', '') == values.c.title)
+
 
 def find_local_path(session, query, latest=None, format='file', **kwargs):
     """
@@ -162,6 +170,7 @@ def find_local_path(session, query, latest=None, format='file', **kwargs):
     else:
         raise NotImplementedError
 
+
 def find_missing_id(session, query, latest=None, format='file', **kwargs):
     """
     Returns the ESGF id for each file in the ESGF query that doesn't have a
@@ -182,4 +191,3 @@ def find_missing_id(session, query, latest=None, format='file', **kwargs):
                 .distinct())
     else:
         raise NotImplementedError
-
