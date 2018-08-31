a1a73ef2b6c97b309e6ba021e1f0422f52207595
Scott Wales <scott.wales@unimelb.edu.au>
2018-08-31 14:56:32 +1000

    Fix path

--- conda/run_test_coverage.sh
+++ conda/run_test_coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pip install coverage pytest-cov codacy-coverage
+pip install coverage pytest-cov
 py.test --cov=clef --cov-report xml:/tmp/artefacts/pytest/coverage.xml --junit-xml /tmp/artefacts/pytest/results.xml
 
