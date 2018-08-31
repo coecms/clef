14b0087fc5dd94b8a5109ac8735f88ee7fbbfe3e
Scott Wales <scott.wales@unimelb.edu.au>
2018-08-31 14:53:17 +1000

    Save results as artefact

--- conda/run_test_coverage.sh
+++ conda/run_test_coverage.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 pip install coverage pytest-cov codacy-coverage
-py.test --cov=clef --cov-report xml
-python-codacy-coverage -r coverage.xml
+py.test --cov=clef --cov-report xml:/tmp/artefacts/pytest/coverage.xml --junit-xml /tmp/artefacts/pytest/results.xml
 
